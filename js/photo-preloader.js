f9cd2b58edf58f0310dbcedeed3dba943e31d8af
Jessica Blue <jessicajanetblue@gmail.com>
2015-10-04 13:30:26 -0700

    changed About section. added image preloading code. made image size smaller and removed unnecessary image files.

---
+++ js/photo-preloader.js
@@ -0,0 +1,25 @@
+<!--//--><![CDATA[//><!--
+	var images = new Array()
+	function preload() {
+		for(i = 0; i < preload.arguments.length; i++) {
+			images[i] = new Image()
+			images[i].src = preload.arguments[i]
+		}
+	}
+	preload(
+		"./images/collage/ink-sketches.png",
+		"./images/collage/hex-skull.png",
+		"./images/collage/poppies.png",
+		"./images/collage/Windows.png",
+		"./images/collage/dubrovnik.png",
+		"./images/collage/grapefruits-1.png",
+		"./images/collage/life-collage.png",
+		"./images/collage/grapefruits-2.png",
+		"./images/collage/caryatids-porch.png",
+		"./images/collage/union-street-aberdeen.png",
+		"./images/collage/Voyeurism.png",
+		"./images/collage/grapefruits-3.png",
+		"./images/collage/hex-flowers.png",
+		"./images/collage/highland-sheep.png"
+	)
+//--><!]]>
